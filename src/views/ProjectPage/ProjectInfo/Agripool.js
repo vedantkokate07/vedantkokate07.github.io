1ab2efdb1cdc34625a073b6ca6ed8a7fd4113a9a
vedantkokate07 <vedant.kokate@somaiya.edu>
2021-08-01 22:25:32 +0530

    Adding AgriPool

---
+++ src/views/ProjectPage/ProjectInfo/Agripool.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+import Camera from "@material-ui/icons/Camera";
+import Palette from "@material-ui/icons/Palette";
+import Favorite from "@material-ui/icons/Favorite";
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import Button from "components/CustomButtons/Button.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import NavPills from "components/NavPills/NavPills.js";
+import Parallax from "components/Parallax/Parallax.js";
+import IconButton from "@material-ui/core/IconButton";
+import { Apps, LinkedIn, Instagram, GitHub } from "@material-ui/icons";
+import profile from "assets/img/faces/vedant.jpeg";
+
+import agri1 from "assets/img/Agripool/3.jpeg";
+import agri2 from "assets/img/Agripool/5.jpeg";
+import agri3 from "assets/img/Agripool/6.jpeg";
+import agri4 from "assets/img/Agripool/7.jpeg";
+import agri5 from "assets/img/Agripool/8.jpeg";
+import agri6 from "assets/img/Agripool/9.jpeg";
+import agri7 from "assets/img/Agripool/10.jpeg";
+
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+
+import styles from "assets/jss/material-kit-react/views/profilePage.js";
+
+const useStyles = makeStyles(styles);
+export default function Agripool(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+  return (
+    <div>
+      <GridContainer container spacing={6} justify="center">
+        <GridItem xs={4} sm={3} md={1}>
+          <img alt="..." src={agri1} className={navImageClasses} />
+        </GridItem>
+        <GridItem xs={4} sm={3} md={1}>
+          <img alt="..." src={agri2} className={navImageClasses} />
+        </GridItem>
+        <GridItem xs={4} sm={3} md={1}>
+          <img alt="..." src={agri3} className={navImageClasses} />
+        </GridItem>
+        <GridItem xs={4} sm={3} md={1}>
+          <img alt="..." src={agri4} className={navImageClasses} />
+        </GridItem>
+        <GridItem xs={4} sm={3} md={1}>
+          <img alt="..." src={agri5} className={navImageClasses} />
+        </GridItem>
+        <GridItem xs={4} sm={3} md={1}>
+          <img alt="..." src={agri6} className={navImageClasses} />
+        </GridItem>
+        <GridItem xs={4} sm={3} md={1}>
+          <img alt="..." src={agri7} className={navImageClasses} />
+        </GridItem>
+      </GridContainer>
+      
+      <GridContainer
+        justifyContent="center"
+        alignItems="center"
+        justify="center"
+      >
+        <GridItem xs={11} sm={8} md={4}><Card>
+        <CardHeader color="danger"> Project description</CardHeader>
+        <CardBody>
+          Agripool is a cross-platform native application. This is an app that
+          connects the farmers to the transporters. The aim was to make sure
+          that a transporter's truck never leaves half filled from the village
+          when going to the wholesale market. This ensures significant gains to
+          the transporters and reduced transportation expenses for the farmers.
+          In simple terms, it is uberpool for farmers.
+        </CardBody>
+      </Card></GridItem>
+      <GridItem xs={11} sm={8} md={4}><Card>
+        <CardHeader color="danger"> My work</CardHeader>
+        <CardBody>
+          My team used flutter to build this application. 
+          I worked on multiple pages. Most of the work done here was on front-end.
+          I, along with my teammates (<a href="https://www.linkedin.com/in/mohil-khare/">Mohil</a>, 
+          <a href="https://www.linkedin.com/in/jaideep-more-a9733015a/"> Jaideep</a>,
+          <a href="https://www.linkedin.com/in/keshav-mishra-912728173/"> Keshav</a>, ) participated 
+          in VCET hackathon with this project. We were the 2nd runner up. 
+          Refer the github for more info.
+          <IconButton><GitHub color="primary" onClick={() => window.open('https://github.com/Phoenix009/AgriPool')} href="https://material-ui.com/api/icon/"/></IconButton>
+        </CardBody>
+      </Card></GridItem>
+      </GridContainer>
+    </div>
+  );
+}
--- src/views/ProjectPage/ProjectPage.js
+++ src/views/ProjectPage/ProjectPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
@@ -17,7 +18,7 @@ import HeaderLinks from "components/Header/HeaderLinks.js";
 import NavPills from "components/NavPills/NavPills.js";
 import Parallax from "components/Parallax/Parallax.js";
 
-import profile from "assets/img/faces/christian.jpg";
+import profile from "assets/img/faces/vedant.jpeg";
 
 import studio1 from "assets/img/examples/studio-1.jpg";
 import studio2 from "assets/img/examples/studio-2.jpg";
@@ -30,11 +31,15 @@ import work3 from "assets/img/examples/cynthia-del-rio.jpg";
 import work4 from "assets/img/examples/mariya-georgieva.jpg";
 import work5 from "assets/img/examples/clem-onojegaw.jpg";
 
+import Agripool from "./ProjectInfo/Agripool";
+import CMS from "./ProjectInfo/CMS"
+
+
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
 
 const useStyles = makeStyles(styles);
 
-export default function ProfilePage(props) {
+export default function ProjectPage(props) {
   const classes = useStyles();
   const { ...rest } = props;
   const imageClasses = classNames(
@@ -45,63 +50,69 @@ export default function ProfilePage(props) {
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
   return (
     <div>
-      <Header
+      {/* <Header
         color="transparent"
         brand="Material Kit React"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
           height: 200,
-          color: "dark",
+          color: "transparent",
         }}
         {...rest}
-      />
+      /> */}
       <Parallax
         small
         filter
         image={require("assets/img/profile-bg.jpg").default}
       />
       <div className={classNames(classes.main, classes.mainRaised)}>
-        <div>
-          <div className={classes.container}>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={6}>
+        <div >
+          <div >
+            <GridContainer justify="center" >
+              <GridItem xs={12} sm={12} md={100} lg={100}>
                 <div className={classes.profile}>
                   <div>
-                    <img src={profile} alt="..." className={imageClasses} />
+                    <img src={profile}   width="102" height="42" className={imageClasses} />
                   </div>
                   <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
-                    <h6>DESIGNER</h6>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-twitter"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-instagram"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-facebook"} />
-                    </Button>
+                    <h3 className={classes.title}>Vedant Kokate</h3>
+         
                   </div>
                 </div>
               </GridItem>
             </GridContainer>
             <div className={classes.description}>
               <p>
-                An artist of considerable range, Chet Faker — the name taken by
-                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-                and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.{" "}
+                I have worked on various projects, 
+                here is the details of my previous projects as well as my current projects.
               </p>
             </div>
             <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+              <GridItem xs={12} sm={12} md={12}  className={classes.navWrapper}>
                 <NavPills
                   alignCenter
-                  color="primary"
+                  color="info"
+                  verticle={{
+                    tabsGrid: { xs: 1, sm: 1, md: 1, size:"small"},
+                    contentGrid: { xs: 12, sm: 8, md: 8 },
+                  }}
                   tabs={[
                     {
-                      tabButton: "Studio",
+                      tabButton: "AgriPool",
+                      tabIcon: Camera,
+                      tabContent: (
+                        <Agripool/>
+                      ),
+                    },
+                    {
+                      tabButton: "CMS",
+                      tabIcon: Favorite,
+                      tabContent: (<CMS/>
+                      ),
+                    },
+                    {
+                      tabButton: "Auditory Analysis Using DRL",
                       tabIcon: Camera,
                       tabContent: (
                         <GridContainer justify="center">
@@ -132,8 +143,9 @@ export default function ProfilePage(props) {
                         </GridContainer>
                       ),
                     },
+                    
                     {
-                      tabButton: "Work",
+                      tabButton: "PredictQ",
                       tabIcon: Palette,
                       tabContent: (
                         <GridContainer justify="center">
@@ -170,7 +182,7 @@ export default function ProfilePage(props) {
                       ),
                     },
                     {
-                      tabButton: "Favorite",
+                      tabButton: "Firmware for Student Sat",
                       tabIcon: Favorite,
                       tabContent: (
                         <GridContainer justify="center">
@@ -206,6 +218,7 @@ export default function ProfilePage(props) {
                         </GridContainer>
                       ),
                     },
+                  
                   ]}
                 />
               </GridItem>
